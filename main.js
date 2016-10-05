9ab1a31890305c12e4148381526d25d20fdb490b
Taylor Sturtz <taylorsturtz.com>
2016-10-04 18:49:44 -0700

    Memory Match v0.5 - Taylor Sturtz

--- main.js
+++ main.js
@@ -5,14 +5,35 @@ var match_counter = 0;
 
 function card_clicked() {
     console.log('card_clicked called');
-
-    first_card_clicked = this;
-
-    if (first_card_clicked == null){
-
+    $(this).hide(); //reveals card
+    if (first_card_clicked === null) {
+        console.log('card is null');
+        first_card_clicked = this;
+    } else {
+        console.log('card is not null');
+        second_card_clicked = this;
+        if ($(first_card_clicked).parent().find('.front img').attr('src') == $(second_card_clicked).parent().find('.front img').attr('src')) {
+            console.log('there is a match');
+            match_counter++;
+            first_card_clicked = null;
+            second_card_clicked = null;
+            if (match_counter == total_possible_matches) {
+                var h1 = $('<h1>').text('You won!');
+                $('.game_inner').append(h1);
+            }
+        } else {
+            setTimeout(flip_back, 2000)
+        }
     }
 }
 
-$(document).ready(function(){
-    $('.card').click(card_clicked);
+function flip_back() {
+    //$('.back').show();
+    $(first_card_clicked, second_card_clicked).parent().parent().find('.back').show();
+    first_card_clicked = null;
+    second_card_clicked = null;
+}
+
+$(document).ready(function () {
+    $('.back').click(card_clicked);
 });
