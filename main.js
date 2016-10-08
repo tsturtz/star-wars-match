893137f608dc8fc2f020121f4a4512532c240432
Taylor Sturtz <taylorsturtz.com>
2016-10-07 19:54:21 -0700

    Random images code added

--- main.js
+++ main.js
@@ -7,6 +7,26 @@ var attempts = 0; //Every time a user attempts a match (clicks the 2nd card) the
 var accuracy = 0; //Accuracy is defined as a percentage of matches / attempts
 var games_played = 0; //When the page is loaded a new global variable should be defined called games_played. When the game is reset by clicking the reset button the games_played should be incremented by 1.
 
+var images_array = [
+    'https://images-na.ssl-images-amazon.com/images/I/411X3hGkeaL._SY300_.jpg', //r2d2
+    'https://s-media-cache-ak0.pinimg.com/564x/8a/db/3f/8adb3fb7f97b05371256bba58ebbde14.jpg', //obiwan
+    'https://static1.squarespace.com/static/55bdd8e1e4b003dc5b7da717/5628c641e4b0629aedbd315b/5628c646e4b02a1b09bf0d2f/1445512774751/C-3PO-See-Threepio_68fe125c.jpeg?format=300w', //c3po
+    'https://s-media-cache-ak0.pinimg.com/564x/f5/a6/d5/f5a6d59d715b1e416d41d5a17040e553.jpg', //gonk droid
+    'http://i.imgur.com/QhLHu0z.jpg', //imperial probe droid
+    'http://www.absolutehobbyz.com/thumbnail.asp?file=assets/images/obxr001usa.jpg&maxx=300&maxy=0', //bb8
+    'http://orig08.deviantart.net/8440/f/2012/080/b/c/profile_picture_by_cw_b1_droid-d4tijnk.jpg', //prequel droid
+    'http://3dsmolier.com/media/cache/a6/69/a669febde530392f3767698682fe7a8d.jpg', //mouse droid
+    'https://duckduckgo.com/i/35ae1b25.jpg' //r4
+];
+
+var all_images = images_array.concat(images_array);
+console.log(all_images);
+while (all_images.length){
+    i = Math.floor(Math.random()*all_images.length);
+    all_images.splice(i,1);
+    console.log('New image : ' + all_images[i]);
+}
+
 function display_stats() {
     accuracy_percentage = accuracy + '%';
     $('.games-played .value').text(games_played);
