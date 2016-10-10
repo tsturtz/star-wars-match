48a09d85b4935de9544534c0c2be2d77d258693d
Taylor Sturtz <taylorsturtz.com>
2016-10-09 22:49:56 -0700

    tinkering

--- main.js
+++ main.js
@@ -19,19 +19,32 @@ var attempts = 0; //Every time a user attempts a match (clicks the 2nd card) thi
 var accuracy = 0; //Accuracy is defined as a percentage of matches / attempts.
 var games_played = 0; //Total games played during a user session.
 
+//Front of card images in an array.
+var images_array = [
+    'https://images-na.ssl-images-amazon.com/images/I/411X3hGkeaL._SY300_.jpg', //r2d2
+    'https://s-media-cache-ak0.pinimg.com/564x/8a/db/3f/8adb3fb7f97b05371256bba58ebbde14.jpg', //obiwan
+    'https://static1.squarespace.com/static/55bdd8e1e4b003dc5b7da717/5628c641e4b0629aedbd315b/5628c646e4b02a1b09bf0d2f/1445512774751/C-3PO-See-Threepio_68fe125c.jpeg?format=300w', //c3po
+    'https://s-media-cache-ak0.pinimg.com/564x/f5/a6/d5/f5a6d59d715b1e416d41d5a17040e553.jpg', //gonk droid
+    'http://i.imgur.com/QhLHu0z.jpg', //imperial probe droid
+    'http://www.absolutehobbyz.com/thumbnail.asp?file=assets/images/obxr001usa.jpg&maxx=300&maxy=0', //bb8
+    'http://orig08.deviantart.net/8440/f/2012/080/b/c/profile_picture_by_cw_b1_droid-d4tijnk.jpg', //prequel droid
+    'http://3dsmolier.com/media/cache/a6/69/a669febde530392f3767698682fe7a8d.jpg', //mouse droid
+    'https://duckduckgo.com/i/35ae1b25.jpg' //r4
+];
+
 //Append all card containers - TODO: Make this more efficient if possible
 function append_all_cards() {
     for (i = 0; i < 6; i++) {
         var card_container = $('<div>').addClass('card col-md-2');
-        $('.row_one').append(card_container);
+        $('.row_1').append(card_container);
     }
     for (i = 0; i < 6; i++) {
         var card_container = $('<div>').addClass('card col-md-2');
-        $('.row_two').append(card_container);
+        $('.row_2').append(card_container);
     }
     for (i = 0; i < 6; i++) {
         var card_container = $('<div>').addClass('card col-md-2');
-        $('.row_three').append(card_container);
+        $('.row_3').append(card_container);
     }
 }
 
@@ -61,47 +74,15 @@ function append_all_fronts() {
 
 //Append all front images to front divs
 function append_all_front_imgs() {
+    //Double images in the above array.
+    var all_images = images_array.concat(images_array);
     $('.front').each(function () {
-        //Front of card images in an array.
-        var images_array = [
-            'https://images-na.ssl-images-amazon.com/images/I/411X3hGkeaL._SY300_.jpg', //r2d2
-            'https://s-media-cache-ak0.pinimg.com/564x/8a/db/3f/8adb3fb7f97b05371256bba58ebbde14.jpg', //obiwan
-            'https://static1.squarespace.com/static/55bdd8e1e4b003dc5b7da717/5628c641e4b0629aedbd315b/5628c646e4b02a1b09bf0d2f/1445512774751/C-3PO-See-Threepio_68fe125c.jpeg?format=300w', //c3po
-            'https://s-media-cache-ak0.pinimg.com/564x/f5/a6/d5/f5a6d59d715b1e416d41d5a17040e553.jpg', //gonk droid
-            'http://i.imgur.com/QhLHu0z.jpg', //imperial probe droid
-            'http://www.absolutehobbyz.com/thumbnail.asp?file=assets/images/obxr001usa.jpg&maxx=300&maxy=0', //bb8
-            'http://orig08.deviantart.net/8440/f/2012/080/b/c/profile_picture_by_cw_b1_droid-d4tijnk.jpg', //prequel droid
-            'http://3dsmolier.com/media/cache/a6/69/a669febde530392f3767698682fe7a8d.jpg', //mouse droid
-            'https://duckduckgo.com/i/35ae1b25.jpg' //r4
-        ];
-        //Double images in the above array.
-        var all_images = images_array.concat(images_array);
-        //Randomize all_images.
-        while (all_images.length) {
-            var i = Math.floor(Math.random() * all_images.length);
-            var front_img_src = all_images[i];
-            var front_card_img = $('<img>').attr('src', front_img_src);
-            all_images.splice(i, 1);
-        }
+        var i = Math.floor(Math.random() * all_images.length);
+        var front_img_src = all_images[i];
+        var front_card_img = $('<img>').attr('src', front_img_src);
+        all_images.splice(i, 1);
         $(this).append(front_card_img);
     });
-}
-
-//DISPLAY STATS
-function display_stats() {
-    accuracy_percentage = accuracy + '%';
-    $('.games-played .value').text(games_played);
-    $('.attempts .value').text(attempts);
-    $('.accuracy .value').text(accuracy_percentage);
-}
-
-//RESET STATS
-function reset_stats() {
-    accuracy = 0;
-    matches = 0;
-    match_counter = 0;
-    attempts = 0;
-    display_stats();
 }
 
 //CARD CLICK
@@ -154,7 +135,23 @@ function flip_back() {
 function reset_button() {
     games_played++;
     reset_stats();
-    display_stats();
     $('.win-screen').remove(); //Hides the win screen if it's active
     $('.back').show(); //Flips all cards to starting position
+}
+
+//RESET STATS
+function reset_stats() {
+    accuracy = 0;
+    matches = 0;
+    match_counter = 0;
+    attempts = 0;
+    display_stats();
+}
+
+//DISPLAY STATS
+function display_stats() {
+    accuracy_percentage = accuracy + '%';
+    $('.games-played .value').text(games_played);
+    $('.attempts .value').text(attempts);
+    $('.accuracy .value').text(accuracy_percentage);
 }
